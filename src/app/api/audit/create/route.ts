06b34b8b99e6250961902669ec1019f59de4f9b2
jkmd96 <joshlinnas@gmail.com>
2023-11-12 23:00:56 +0300

    update staging page

---
+++ src/app/api/audit/create/route.ts
@@ -0,0 +1,25 @@
+import {NextRequest} from "next/server";
+import {handleAxiosResponse, handleApiException} from "@/helpers/responseHelpers";
+import adminApiClient from "@/lib/axios/axiosClient";
+import {cookieName} from "@/boundary/constants/appConstants";
+import {AxiosRequestConfig} from "axios";
+
+export async function POST(request: NextRequest) {
+    try {
+        const tokenCookie = request.cookies.get(`${cookieName}`)?.value as string;
+        const {accessToken} = JSON.parse(tokenCookie);
+
+        const config: AxiosRequestConfig = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        };
+        const requestBody = await request.text();
+        const response = await adminApiClient
+            .post('staging-records', `${requestBody}`,config);
+
+        return handleAxiosResponse(response);
+    } catch (error: unknown) {
+        return handleApiException(error);
+    }
+}
---
+++ src/app/api/audit/list/route.ts
@@ -0,0 +1,40 @@
+import {handleAxiosResponse, handleApiException} from "@/helpers/responseHelpers";
+import adminApiClient from "@/lib/axios/axiosClient";
+import {NextRequest} from "next/server";
+import {cookieName} from "@/boundary/constants/appConstants";
+import {AxiosRequestConfig} from "axios";
+
+export async function GET(request: NextRequest) {
+    try {
+        const tokenCookie = request.cookies.get(`${cookieName}`)?.value as string;
+        const {accessToken} = JSON.parse(tokenCookie);
+
+        const url = new URL(request.url)
+        const searchParams = new URLSearchParams(url.search);
+        const pageSize = searchParams.get('pageSize');
+        const pageNumber = searchParams.get('pageNumber');
+        const orderBy = searchParams.get('orderBy');
+        const searchTerm = searchParams.get('searchTerm');
+        const action = searchParams.get('action');
+        const status = searchParams.get('status');
+        const config: AxiosRequestConfig = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            },
+            params: {
+                pageSize,
+                pageNumber,
+                orderBy,
+                searchTerm,
+                status,
+                action
+            }
+        };
+
+        const response = await adminApiClient.get('staging-records', config);
+        console.log("fetch staging response", response.data);
+        return handleAxiosResponse(response);
+    } catch (error: unknown) {
+        return handleApiException(error);
+    }
+}
